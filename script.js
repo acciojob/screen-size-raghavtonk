00125f951fd2eb1cd6d051d269e141c7dced84a6
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-03-02 00:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+window.onload = function(){
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	text.textContent=`Width: ${width} and Height: ${height}`
+}
+window.onresize= function(){
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	text.textContent=`Width: ${width} and Height: ${height}`
+}
